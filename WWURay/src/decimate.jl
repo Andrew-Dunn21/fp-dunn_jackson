1f60c18cb804eead41e3052aa396f62e0f47f879
alec-jackson <jackso80@wwu.edu>
2021-03-05 17:03:21 -0800

    Added viable edge tracking and sorting

--- WWURay/src/decimate.jl
+++ WWURay/src/decimate.jl
@@ -7,26 +7,46 @@ using ..WWUMeshes
 
 mutable struct decimationInfo
     edges::Array{Array{Int, 1}, 1} # A list of all the unique edges
+    edgeDist::Array{Array{Float64, 1}, 1} # a list of edges sorted by their euclidean distance
 end
 
 function decimate()#mesh::OBJMesh)
+    println("Loading Mesh")
     mesh = read_obj("data/bunny.obj")
+    println("Finding Edges")
     decimator = findEdges(mesh)
     println(size(decimator.edges)[1], " Edges")
+    buildViable(mesh, decimator)
 
+    return 1
     #return mesh
 end
 
-function buildViable(mesh::OBJMesh)
-
+function buildViable(mesh::OBJMesh, decimator::decimationInfo)
+    println("Building Viable Edges")
+    i = 1
+    for edge in decimator.edges
+        d = calcDistance(mesh.positions[edge[1]], mesh.positions[edge[2]])
+        push!(decimator.edgeDist, [d,i])
+        i += 1
+    end
+    decimator.edgeDist = sort(decimator.edgeDist)
+    #println(decimator.edgeDist)
+    return decimator
 end
 
-function calcDistance(point::Vec3)
+function calcDistance(point1::Vec3, point2::Vec3)
+    diffPoint = point2 - point1
+    d = diffPoint[1]^2 + diffPoint[2]^2 + diffPoint[3]^2
+    return d
+end
+
+function removeDups(mesh::OBJMesh, decimator::decimationInfo)
 
 end
 
 function findEdges(mesh::OBJMesh)
-    decimator = decimationInfo([])
+    decimator = decimationInfo([], [[0.0]]::Array{Array{Float64, 1}, 1})
     j = 0
 
     for triangles in mesh.triangles
